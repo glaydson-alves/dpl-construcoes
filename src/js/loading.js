d136201b2b8cc8dc1c8318b9a6409a496d96bd72
glaydson-alves <glaydson.alves36@gmail.com>
2023-09-20 23:48:28 -0300

    loading js manuteção

--- src/js/loading.js
+++ src/js/loading.js
@@ -4,12 +4,5 @@ const enviarButton = document.getElementById('submit')
         enviarButton.addEventListener('click', function() {
             
             enviarButton.style.display = 'none'
-            enviarButton.innerHTML = ''
             loader.style.display = 'block'
-
-            setTimeout(function() {
-                enviarButton.style.display = 'block'
-                enviarButton.innerHTML = 'ENVIAR'
-                loader.style.display = 'none'
-            }, 3000); 
         });
